0a305dea74611bdb29b149d619b765cfbf1bda5d
hiramrr <sasuke_77.23@hotmail.com>
2025-11-20 23:27:03 -0600

    inicio del proyecto

---
+++ src/main/java/miyu/smart/BackendMusica/BackendMusicaApplication.java
@@ -0,0 +1,13 @@
+package miyu.smart.BackendMusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miyu/smart/BackendMusica/BackendMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package miyu.smart.BackendMusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
