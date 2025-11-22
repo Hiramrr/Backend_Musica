1d70504927d09a3974d10aee6626bb4c91facd8d
hiramrr <sasuke_77.23@hotmail.com>
2025-11-22 14:56:59 -0600

    reseña-entidad

--- src/main/java/miyu/smart/BackendMusica/entity/Reseña.java
+++ src/main/java/miyu/smart/BackendMusica/entity/Reseña.java
@@ -1,15 +1,11 @@
 package miyu.smart.BackendMusica.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "reseña")
-@Data
-@NoArgsConstructor
 public class Reseña {
 
     @Id
@@ -37,4 +33,74 @@ public class Reseña {
     @ManyToOne
     @JoinColumn(name = "cancion_id", nullable = true)
     private Cancion cancion;
+
+    public Reseña(UUID id, String contenido, Double calificacion, LocalDateTime fechaCreacion, Album album, Usuario usuario, Cancion cancion) {
+        this.id = id;
+        this.contenido = contenido;
+        this.calificacion = calificacion;
+        this.fechaCreacion = fechaCreacion;
+        this.album = album;
+        this.usuario = usuario;
+        this.cancion = cancion;
+    }
+
+    public Reseña() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    public Cancion getCancion() {
+        return cancion;
+    }
+
+    public void setCancion(Cancion cancion) {
+        this.cancion = cancion;
+    }
 }
