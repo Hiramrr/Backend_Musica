ca52f9fe68ede4a84b34013dfa5e38963985e109
hiramrr <sasuke_77.23@hotmail.com>
2025-11-23 10:31:18 -0600

    esquema mejorado

--- esquema_sql/esquema.sql
+++ esquema_sql/esquema.sql
@@ -1,74 +1,80 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-create table usuario (
-                         id UUID PRIMARY KEY,
-                         nombre varchar(255) not null,
-                         correo varchar(255) not null unique,
-                         foto_url varchar(255),
-                         password varchar(255) not null
+DROP TABLE IF EXISTS reseña;
+DROP TABLE IF EXISTS album_artistas;
+DROP TABLE IF EXISTS cancion_artistas;
+DROP TABLE IF EXISTS cancion;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS artista;
+DROP TABLE IF EXISTS usuario;
+
+CREATE TABLE usuario (
+    id UUID PRIMARY KEY,
+    nombre varchar(255) NOT NULL,
+    correo varchar(255) NOT NULL UNIQUE,
+    foto_url varchar(255),
+    password varchar(255) NOT NULL
 );
 
-create table artista (
-                         id UUID primary key,
-                         nombre varchar(255) not null,
-                         fecha_inicio int not null,
-                         fecha_fin int,
-                         nacimiento date,
-                         bibiliografia text,
-                         foto_url varchar(255)
+CREATE TABLE artista (
+    id UUID PRIMARY KEY,
+    nombre varchar(255) NOT NULL,
+    fecha_inicio int NOT NULL,
+    fecha_fin int,
+    nacimiento date,
+    bibliografia text,
+    foto_url varchar(255)
 );
 
-create table album (
-                       id UUID primary key,
-                       nombre varchar(255) not null,
-                       descripcion text,
-                       duracion_segundos int not null,
-                       calificacion double precision,
-                       total_canciones int,
-                       fecha_salida int,
-                       portada_url varchar(255)
+CREATE TABLE album (
+    id UUID PRIMARY KEY,
+    nombre varchar(255) NOT NULL,
+    descripcion text,
+    duracion_segundos int NOT NULL,
+    calificacion_promedio double precision,
+    total_canciones int,
+    fecha_salida int,
+    portada_url varchar(255)
 );
 
+CREATE TABLE cancion (
+    id UUID PRIMARY KEY,
+    nombre varchar(255) NOT NULL,
+    descripcion text,
+    duracion_segundos int NOT NULL,
+    fecha_salida int,
+    calificacion double precision,
+    portada_url varchar(255),
 
-create table cancion (
-                         id UUID primary key,
-                         nombre varchar(255) not null,
-                         descripcion text,
-                         duracion_segundos int not null,
-                         album_id uuid references album(id) on delete cascade,
-                         fecha_salida int,
-                         calificacion double precision,
-                         portada_url varchar(255)
+    id_album uuid REFERENCES album(id) ON DELETE CASCADE
 );
 
-create table cancion_artistas(
-                                 id_cancion uuid references cancion(id) on delete cascade,
-                                 id_artista uuid references artista(id) on delete cascade,
-
-                                 PRIMARY KEY (id_cancion, id_artista)
+CREATE TABLE cancion_artistas(
+    id_cancion uuid REFERENCES cancion(id) ON DELETE CASCADE,
+    id_artista uuid REFERENCES artista(id) ON DELETE CASCADE,
+    PRIMARY KEY (id_cancion, id_artista)
 );
 
-create table album_artistas(
-                               id_album uuid references album(id) on delete cascade,
-                               id_artista uuid references artista(id) on delete cascade,
-
-                               PRIMARY KEY (id_album, id_artista)
+CREATE TABLE album_artistas(
+    id_album uuid REFERENCES album(id) ON DELETE CASCADE,
+    id_artista uuid REFERENCES artista(id) ON DELETE CASCADE,
+    PRIMARY KEY (id_album, id_artista)
 );
 
-create table reseña(
-                       id uuid primary key,
-                       contenido TEXT not null,
-                       calificacion DOUBLE PRECISION not null,
-                       fecha_creacion date,
-                       id_usuario uuid references usuario(id) on delete cascade not null,
-                       id_cancion uuid references cancion(id) on delete cascade,
-                       id_album uuid references album(id) on delete cascade
+CREATE TABLE reseña(
+    id uuid PRIMARY KEY,
+    contenido TEXT NOT NULL,
+    calificacion DOUBLE PRECISION NOT NULL,
+    fecha_creacion date,
+
+    id_usuario uuid NOT NULL REFERENCES usuario(id) ON DELETE CASCADE,
+    id_cancion uuid REFERENCES cancion(id) ON DELETE CASCADE,
+    id_album uuid REFERENCES album(id) ON DELETE CASCADE,
+
+    CONSTRAINT check_solo_uno CHECK (
+    (id_album IS NOT NULL AND id_cancion IS NULL) OR
+    (id_album IS NULL AND id_cancion IS NOT NULL)
+    )
 );
 
---- proteccion de que la reseña solo sea a un album pero no a una cancion y al reves jaja
-ALTER TABLE reseña
-    ADD CONSTRAINT check_solo_uno
-        CHECK (
-            (id_album IS NOT NULL AND id_cancion IS NULL) OR
-            (id_album IS NULL AND id_cancion IS NOT NULL)
-            );
+select * from cancion;
