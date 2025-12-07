bfbacd9248130a5be0af5b0d43a18b838cac381d
rodrigoHol <rodrigokum091@gmail.com>
2025-12-07 14:29:19 -0600

    Quitando bloqueo de CORS

--- src/main/java/miyu/smart/BackendMusica/config/Seguridad.java
+++ src/main/java/miyu/smart/BackendMusica/config/Seguridad.java
@@ -1,7 +1,5 @@
 package miyu.smart.BackendMusica.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +7,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +18,25 @@ public class Seguridad {
 
     @Bean
     public SecurityFilterChain fitroSeguridad(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http
+            .cors(cors -> cors.configurationSource(apiConfigurationSource()))
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
+    }
+
+    //Definimos quién tiene permiso para conectarse y que peticions http puede hacer
+    @Bean
+    public CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); 
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        // Se puede enviar cualquier cabecera para poder mandar JSON desde el frontend
+        configuration.setAllowedHeaders(List.of("*"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
